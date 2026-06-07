f66e33d8dd788fb2fbb79a2ce2e2920e3e093d3b
Rayssa Vicente da Silva Viegas <102470604+RayssaVicente@users.noreply.github.com>
2026-06-07 12:06:52 -0300

    fix: adicionando a ligação com o backend

--- services/api.ts
+++ services/api.ts
@@ -4,7 +4,7 @@ import { CustomerData } from '@/app/interfaces/CustomerData';
 import {PaymentData} from '@/app/interfaces/PaymentData'    
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL
+  baseURL: "https://backend-food-ecommerce.onrender.com"
 });
 
 export const processCheckout = (cart: Product[], customer: CustomerData, payment: PaymentData) => api.post('/checkout', {
